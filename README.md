627cf413a1cdf80179b404bba60e80f92778b7a8
tina042 <135068124+tina042@users.noreply.github.com>
2023-06-20 14:13:40 -0400

    project done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# slitherin-proj
+## Language 💻
+
+- HTML
+- CSS
+- JavaScript
+
+(No External Library Used)
+
+## Features ✨
+
+- Authentication process with API.
+- Cool multilevel Navigation bar.
+- Cool CSS over pages of website
---
+++ signupPage/login.js
@@ -0,0 +1,42 @@
+document.querySelector("#crossSign").addEventListener("click" , function(){
+    window.location.href = "signup.html"
+})
+
+
+let data = JSON.parse(localStorage.getItem("SignUp-Data")) || [];
+
+
+document.querySelector("form").addEventListener("submit",Logindata)
+
+let LoginData = JSON.parse(localStorage.getItem("Login-Data")) || [];
+
+
+
+function Logindata(e){
+    e.preventDefault();
+
+        let email = document.querySelector("#email").value
+        let pass = document.querySelector("#pwd").value
+        let filter=  data.filter(function(e){
+            return (email == e.email && pass == e.password);
+        })
+        if(filter.length>0){
+            localStorage.setItem("Login-Data" , JSON.stringify(filter));
+            openPop()
+        }else{
+            alert("Wrong Credential")
+        }
+        
+}
+
+
+
+function openPop(){
+    let popup = document.querySelector(".popup")
+    popup.classList.add("pop-class");
+}
+function closePop(){
+    let popup = document.querySelector(".popup")
+    popup.classList.remove("pop-class");
+    window.location.href = "../index.html"
+}
